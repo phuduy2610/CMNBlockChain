0f25d04ed97fbc581410fec2e4133ada2c41e746
phuduy2610 <phuduy2610@gmail.com>
2022-04-19 22:42:25 +0700

    - Basic block chain

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const SHA256 = require('crypto-js');
+const SHA256 = require('crypto-js/sha256');
 class Block {
     index;
     timestamp;
     data;
     previousHash;
     hash;
-    constructor( timestamp, data, previousHash = '') {
+    constructor(data, timestamp, previousHash = '') {
         //index: So thu tu trong chuoi
         //timestamp: Thoi gian tao
         //data: Du lieu ( thong tin ve giao dich co the de o day)
@@ -22,29 +22,45 @@ class Block {
     }
 }
 
-class BlockChain{
-    constructor(){
+class BlockChain {
+    constructor() {
         this.chain = [this.createGenesisBlock()];
     }
 
     //Ham khoi tao block dau tien
-    createGenesisBlock(){
-        return new Block(0,"01/01/2022","Genesis block","0");
+    createGenesisBlock() {
+        return new Block("Genesis block", new Date().toLocaleString(), "0");
     }
 
-    getLatestBlock(){
-        return this.chain[this.chain.length-1];
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
     }
-    
-    addNewBlock(newBlock){
+
+    addNewBlock(newBlock) {
         newBlock.index = this.chain.length;
         newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.timestamp = new Date().toLocaleString();
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
+    }
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+            if ((currentBlock.hash != currentBlock.calculateHash()) || previousBlock.hash != currentBlock.previousHash) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
 let octCoin = new BlockChain();
-octCoin.addNewBlock(new Block("19/04/2022",{amount : 4}));
-octCoin.addNewBlock(new Block("22/04/2022",{amount : 10}));
-console.log(JSON.stringify(octCoin,null,4));
+octCoin.addNewBlock(new Block({ amount: 4 }));
+octCoin.addNewBlock(new Block({ amount: 10 }));
+console.log(JSON.stringify(octCoin, null, 4));
+console.log("Is valid:"+ octCoin.isChainValid());
+octCoin.chain[1].data = {amount:1};
+console.log("Is valid now:"+ octCoin.isChainValid());
+
